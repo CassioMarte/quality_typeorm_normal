e66a505e0bfce9759cbd8db155aec28a11dc97f4
CassioMarte <135394169+CassioMarte@users.noreply.github.com>
2024-12-28 05:10:56 -0300

    Update User.routes.ts

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -9,3 +9,16 @@ userRoutes.post('/', userController.createUserController);
 userRoutes.get('/', userController.showUserController)
 
 export { userRoutes };
+
+
+import { Router } from 'express';
+import { UserController } from '../controllers/User.Controller';
+
+const userRoutes = Router();
+const userController = new UserController();
+
+// Usar funções anonimas para garantir o contexto correto
+userRoutes.post('/', (req, res) => userController.createUserController(req, res));
+userRoutes.get('/', (req, res) => userController.showUserController(req, res));
+
+export default userRoutes;
